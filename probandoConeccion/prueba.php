995e3d1465432c5cff980a66699b3945f2a32780
Micaela-Yanet-Soto <micaela.soto@est.fi.uncoma.edu.ar>
2021-03-17 19:33:41 -0300

    nuevo

--- probandoConeccion/prueba.php
+++ probandoConeccion/prueba.php
@@ -1,1 +1,9 @@
 <?php
+echo"Ingrese un numero mayor a 0 y menor que 20 ";
+$num= trim(fgets(STDIN));
+
+if($num>0){
+    echo"El numero se encuentra dentro del rango";
+}else{
+    echo"el numero seleccionado no se encuntra dentro del rango dado";
+}
